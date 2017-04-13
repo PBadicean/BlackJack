0c9f73cbdfedc78f91b2f35b259bbe12eff2fae2
Badichan Polina <badichan@mail.ru>
2017-04-13 10:51:19 +0200

    game fix2

--- gamer.rb
+++ gamer.rb
@@ -3,24 +3,23 @@ require_relative 'deck'
 class Gamer
   GAIN = 20
   HALF = 10
-  LEVY = 10
+  ANTE = 10
 
   attr_accessor :bank, :cards, :deck, :score
 
   def initialize(name)
     @deck = Deck.new
     @bank = 100
-    @database = []
     @cards = []
     @score = 0
   end
 
-  def do_levy
-    @bank -= LEVY
+  def do_ante
+    @bank -= ANTE
   end
 
   def add_card
-    do_card(1)
+    do_card
     card = @cards.last
     if card.include?('tuz')
       if @score > 11
